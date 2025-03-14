cd9d783827f5107032d70e77482d8d4a5fbfe906
cdray527 <cdray085@hotmail.com>
2025-03-14 15:54:20 +0800

    Initial commit

--- apps/frontend/dashboard-e2e/cypress.config.ts
+++ apps/frontend/dashboard-e2e/cypress.config.ts
@@ -2,15 +2,15 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
-      webServerCommands: {
-        default: 'npx nx run @diytaskmanager/dashboard:dev',
-      },
-      ciWebServerCommand: 'npx nx run @diytaskmanager/dashboard:start',
-      ciBaseUrl: 'http://localhost:3000',
-    }),
-    baseUrl: 'http://127.0.0.1:3000',
-  },
+    e2e: {
+        ...nxE2EPreset(__filename, {
+            cypressDir: 'src',
+            webServerCommands: {
+                default: 'npx nx run @diytaskmanager/dashboard:dev'
+            },
+            ciWebServerCommand: 'npx nx run @diytaskmanager/dashboard:start',
+            ciBaseUrl: 'http://localhost:3000'
+        }),
+        baseUrl: 'http://127.0.0.1:3000'
+    }
 });
--- apps/frontend/dashboard/jest.config.ts
+++ apps/frontend/dashboard/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: '@diytaskmanager/dashboard',
-  preset: '../../../jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: 'test-output/jest/coverage',
+    displayName: '@diytaskmanager/dashboard',
+    preset: '../../../jest.preset.js',
+    transform: {
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+        '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }]
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: 'test-output/jest/coverage'
 };
