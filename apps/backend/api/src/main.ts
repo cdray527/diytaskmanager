c378abd7d70aafdb19faf89392ea013a9f76cc47
cdray527 <cdray085@hotmail.com>
2025-03-15 18:59:48 +0800

    Fix hmr in api dev

--- apps/backend/api/src/main.ts
+++ apps/backend/api/src/main.ts
@@ -17,13 +17,14 @@ async function bootstrap() {
         .setVersion('1.0')
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api/docs', app, document);
 
     const globalPrefix = 'api';
     app.setGlobalPrefix(globalPrefix);
     const port = process.env.PORT || 3000;
     await app.listen(port);
     Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`📄 Swagger Docs at http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
