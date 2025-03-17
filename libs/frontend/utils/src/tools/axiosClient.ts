b51f70f405ae6a92237b9795cfcc2f62b5fae0b8
cdray527 <cdray085@hotmail.com>
2025-03-17 15:33:39 +0800

    Debug CICD

--- libs/frontend/utils/src/tools/axiosClient.ts
+++ libs/frontend/utils/src/tools/axiosClient.ts
@@ -17,6 +17,8 @@ const createAxiosClient = (serviceName: ApiServiceName) => {
         ? apiServiceUrlMapper[serviceName].SSR
         : apiServiceUrlMapper[serviceName].CSR;
 
+    console.log(baseURL);
+
     return axios.create({
         baseURL,
         headers: {
